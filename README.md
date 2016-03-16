6500667148bbd58a55a495ccf14715543ac9c7fd
YAO,WEI <gamania54088@gmail.com>
2016-03-16 15:09:54 +0800

    readme.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# php_pdo
-使用pdo來連線mysql
+#php mysqli
 
+因應mysql_connect用法將在php未來的版本中被移除
+
+在這邊提供pdo的連線方法
+
+以及基本的資料庫查詢
+
+|類型                                        |說明                                         |
+|--------------------------------------------|---------------------------------------------|
+|[object](object/)                           |使用object連線                               |
+|[procedural](procedural/)                   |使用procedural連線                           |
---
+++ sample_db.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2016-03-16: 03:35:36
+-- 伺服器版本: 5.6.17
+-- PHP 版本： 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 資料庫： `sample_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `member`
+--
+
+CREATE TABLE IF NOT EXISTS `member` (
+  `usr_id` varchar(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `birthday` date NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 資料表的匯出資料 `member`
+--
+
+INSERT INTO `member` (`usr_id`, `name`, `birthday`, `address`) VALUES
+('S201600001', 'Andy', '2000-02-23', 'X市Y區1號2樓'),
+('S201600002', 'Bill', '2001-01-31', 'X市Z區2號'),
+('S201600003', 'Cindy', '2000-02-03', 'X市Y區2-2號');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
